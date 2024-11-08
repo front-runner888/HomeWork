feec635cc8309b09b105f46a874b8662b3534957
front-runner888 <islamgulovaelvira88@gmail.com>
2024-11-08 13:53:34 +0500

    first commit

---
+++ dz1.js
@@ -0,0 +1,46 @@
+// 1 task
+//  let name=prompt( 'как ваше имя?')
+//  alert('привет '+ name)
+  //2task
+//   const year=2024
+//   let birthYear=prompt('Год вашего рождения: ', 0)
+//   let age=year-birthYear
+//   alert(age)
+  // 3 task
+//   let number=prompt( 'Укажите длину стороны квадрата')
+//   alert(number*4)
+  //4 task
+//   let number1=3.14
+//   let number2=prompt('Радиус окружности')
+//   alert(number1*number2**2)
+//5 task
+// let number1=prompt('скорость')
+// let number2=prompt('время')
+// let number3 = prompt('Найти расстояние')
+// alert(number1*number2)
+// 6 task
+// const euro=0.918 
+// let number1=prompt('доллар')
+// let number2=prompt('сколько евро')
+//  alert(number1*0.918 )
+// 7 task
+// const Gb=1024
+// let number1=prompt('гигабайт')
+// let number2=prompt('сколько файлов')
+// alert((number1*1024)/820)
+//8 task
+// let number1=prompt('сумма денег')
+// let number2= prompt('цена шоколадки')
+// console.log(result= Math.trunc(number1/number2))
+// console.log(change=number1- result*number2)
+// alert(  'можно купить'+ result + 'остаток'+ change)
+//9task
+// let number1 = prompt('Введите трёхзначное число: ')  
+// let a = Number(number1 % 10)
+// let b = Number((number1 - a) / 10 % 10)
+// let c = Number((number1 - a - b * 10) / 100)
+// alert('Перевёртыш: ' + (a * 100 + b * 10 + c))
+//10task
+// let a = prompt('Введите число для проверки на чётность: ')
+// a = Number(a);
+// alert((parseInt(a) % 2 == 0) ? "ЧЁТНОЕ" : "НЕЧЁТНОЕ")
---
+++ dz2.js
@@ -0,0 +1,52 @@
+// 1 task
+// 
+// let age= prompt('Введите ваш возраст')
+//  if(age>=0 && age<=2){
+//   console.log(' Вы ребенок')
+//  }
+//  else if(age>=12 && age<18){
+//   console.log(' Вы подросток')
+//  }
+//  else if(age>=18 && age<60){
+//   console.log(' Вы взрослый')
+//  }
+//  else if(age>60){
+//   console.log(' Вы пенсионер!')
+//  }
+ //2 task
+//  let number=prompt('your number')
+//  if(number<=1){
+//     console.log('!')
+//  }
+//  else if(number<=2){
+//     console.log('@')
+//  }
+//  else if(number<=3){
+//     console.log('#')
+//  }    
+//  else if(number<=4){
+//     console.log('$')
+//  }
+//  else if(number<=5){
+//     console.log('%')
+//  }
+//  else if(number<=6){
+//     console.log('^')
+//  }
+//  else if(number<=7){
+//     console.log('&')
+//  }
+//  else if(number<=8){
+//     console.log('*')
+//  }
+//  else if(number<=9){
+//     console.log('(')
+//  }
+// task3
+let number1= a
+let number2=b
+let number3=c
+let abc=prompt('put in a three-digit number')
+if(a==b && a==c){
+      console.log('there are the same numbers')
+}
