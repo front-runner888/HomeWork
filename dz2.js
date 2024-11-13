f74723af4a2ddb73dd1ef94a96ef010467358873
front-runner888 <islamgulovaelvira88@gmail.com>
2024-11-13 15:22:05 +0500

    Update dz2.js

--- dz2.js
+++ dz2.js
@@ -43,10 +43,46 @@
 //     console.log('(')
 //  }
 // task3
-let number1= a
-let number2=b
-let number3=c
-let abc=prompt('put in a three-digit number')
-if(a==b && a==c){
-      console.log('there are the same numbers')
-}
+// let number= prompt ('put in a three-digit number')
+// let arrNum=number.split('')
+// console.log(number)
+//  console.log(arrNum)
+//  console.log(arrNum[0])
+//  console.log(arrNum[1])
+//  console.log(arrNum[2])
+//  if (arrNum[0]===arrNum[1]){
+//       alert('there is coincidence')
+//  }
+//  else if(arrNum[0]===arrNum[2]){
+//       alert('there is coincidence')
+//  }
+//  else if(arrNum[1]===arrNum[2]){
+//       alert('there is coincidence')
+//  }else{
+//       alert('there is no')
+//  }
+// task 4
+// let  year = prompt('Insert the year')
+// if(year % 4 ==0){
+//       console.log('it is leap year')
+// }
+// else if (year % 400==0){
+//       console.log('it is leap year')
+// }
+// else if(year % 100 !=0){
+//       console.log('it is not a leap year')
+// }
+// task 5
+// let number=prompt('is it palindrom')
+// let arrNum=number.split('')
+// console.log(arrNum[0])
+// console.log(arrNum[1])
+// console.log(arrNum[2])
+// console.log(arrNum[3])
+// console.log(arrNum[4])
+// if (arrNum[0]== arrNum[4] && arrNum[1] == arrNum[3]){
+// console.log ('it is palindrom')
+// }
+// else{
+//       console.log ('it is not')
+//       } 
